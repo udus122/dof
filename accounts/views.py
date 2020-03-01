7b475578b51d91d0fb726d51c3b71c9dfd4de5c7
uyudai1202 <u.yudai1202@gmail.com>
2020-03-01 17:01:24 +0900

    自作テンプレートを読み込むようにViewを作成

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from allauth.account.views import LoginView, LogoutView, SignupView
 
-# Create your views here.
+
+class LoginView(LoginView):
+    """
+    ログインビュー
+    """
+    template_name = 'login.html'
+
+
+login_view = LoginView.as_view()
+
+
+class LogoutView(LogoutView):
+    """
+    ログアウトビュー
+    """
+    template_name = 'logout.html'
+
+
+logout_view = LogoutView.as_view()
+
+
+class SignupView(SignupView):
+    """
+    サインアップビュー
+    """
+    template_name = 'signup.html'
+
+
+signup_view = SignupView.as_view()
--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('accounts/', include('allauth.urls')),
-    path('accounts/login/', TemplateView.as_view(template_name='login.html'), name='login'),
-    path('accounts/logout/', TemplateView.as_view(template_name='logout.html'), name='logout'),
-    path('accounts/signup/', TemplateView.as_view(template_name='signup.html'), name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup_view, name='signup'),
 ]
