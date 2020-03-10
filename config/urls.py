0daf3bc2b505bb303124ae5f29ee4286a9bcc372
uyudai1202 <u.yudai1202@gmail.com>
2020-03-10 14:41:21 +0900

    mypage, diagnosisをURLconfへ追加

--- config/urls.py
+++ config/urls.py
@@ -42,4 +42,6 @@ urlpatterns = [
         name="account_reset_password_from_key"
     ),
     path('password/reset/key/done/', account_views.password_reset_from_key_done_view, name='account_reset_password_from_key_done'),
+    path('mypage/', dof_views.mypage_view, name='mypage'),
+    path('diagnosis/', dof_views.diagnosis_view, name='diagnosis'),
 ]
