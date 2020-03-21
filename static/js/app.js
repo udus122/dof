f701a42f0a7ed1fc85323c16060b420ce0b132be
yutasb <shibamoto.yuta@gmail.com>
2020-03-21 12:07:54 +0900

    マイページ、診断微調整

--- static/js/app.js
+++ static/js/app.js
@@ -8,3 +8,24 @@ var bar = new ProgressBar.Line(progressBar, {
   svgStyle: { width: "100%", height: "100%" }
 });
 bar.animate(1.0);
+
+//（イベントのトリガーはページの更新？どれかのボタンが押されること？）
+//　→　9秒後に「どちらでもない」がclickされる。
+//いずかれの選択肢がクリックされたら次の問題へ行く。(更新される)
+//progressbarも再開される。
+
+var answerIsYes = document.getElementById("answerIsYes");
+var answerIsNeither = document.getElementById("answerIsNeither");
+var answerIsNo = document.getElementById("answerIsNo");
+
+//ページをリロードするための関数
+function doReload() {
+  window.location.reload();
+}
+
+//ページが表示されたら9秒後にリロードする処理
+window.addEventListener("load", function() {
+  setTimeout(doReload, 9000);
+});
+
+answerIsNeither.addEventListener("click", function() {});
